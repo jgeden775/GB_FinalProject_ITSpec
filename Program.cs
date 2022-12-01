e932c91e2f79faf370d9c45fbf09eef7d32071ba
John <jeden667@gmail.com>
2022-12-01 12:34:33 +0400

    Optimized code in Main Program

--- Program.cs
+++ Program.cs
@@ -1,35 +1,28 @@
 ﻿using FuncLib;
 
-public class Program
+Console.Clear();
+
+int length = new Random().Next(1, 3);
+Console.WriteLine("Введите любые наборы слов или чисел, через пробел.");
+Console.WriteLine($"Внимание, должно быть более {length} элементов!");
+Console.Write(">> ");
+string[] input = Func.GetStrArr(Console.ReadLine());
+string[] output = Func.CutStrArr(input, length);
+Console.WriteLine();
+
+if (input.Length <= length)
 {
-    public static void Main()
-    {
-        Console.Clear();
-
-        int length = new Random().Next(1, 3);
-        Console.WriteLine("Введите любые наборы слов или чисел, через пробел.");
-        Console.WriteLine($"Внимание, должно быть более {length} элементов!");
-        Console.Write(">> ");
-        string[] input = Func.GetStrArr(Console.ReadLine());
-        string[] output = Func.CutStrArr(input, length);
-        Console.WriteLine();
-
-        if (input.Length <= length)
-        {
-            Console.WriteLine($"Неверный ввод! Должно быть больше {length} элементов.");
-            Console.WriteLine();
-        }
-        else
-        {
-            Console.WriteLine("Формируем из них массив: ");
-            Console.Write(">> ");
-            Console.WriteLine($"[{String.Join(", ", input)}]");
-            Console.WriteLine();
-            Console.WriteLine($"Сокращаем число элементов до {length}: ");
-            Console.Write(">> ");
-            Console.WriteLine($"[{String.Join(", ", output)}]");
-            Console.WriteLine();
-        }
-
-    }
+    Console.WriteLine($"Неверный ввод! Должно быть больше {length} элементов.");
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("Формируем из них массив: ");
+    Console.Write(">> ");
+    Console.WriteLine($"[{String.Join(", ", input)}]");
+    Console.WriteLine();
+    Console.WriteLine($"Сокращаем число элементов до {length}: ");
+    Console.Write(">> ");
+    Console.WriteLine($"[{String.Join(", ", output)}]");
+    Console.WriteLine();
 }
